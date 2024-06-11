fd9a082eabc5e87b772f9045ae94250f1df659b0
Maira Salazar <47562677+mairasalazar@users.noreply.github.com>
2024-06-11 18:29:02 +0000

    Create dashboard prototype

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,28 +13,78 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# streamlit_app.py
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+import streamlit as st
+import tableauserverclient as TSC
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+# Set up connection.
+tableau_auth = TSC.PersonalAccessTokenAuth(
+    st.secrets["tableau"]["token_name"],
+    st.secrets["tableau"]["token_secret"],
+    st.secrets["tableau"]["site_id"],
+)
+server = TSC.Server(st.secrets["tableau"]["server_url"], use_server_version=True)
+print(server)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# Get various data.
+# Explore the tableauserverclient library for more options.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def fetch_workbooks_and_views():
+    with server.auth.sign_in(tableau_auth):
+        workbooks_data = {}
+        workbooks, pagination_item = server.workbooks.get()
+
+        for workbook in workbooks:
+            server.workbooks.populate_views(workbook)
+            views = workbook.views
+            workbooks_data[workbook.name] = views
+        
+        return workbooks_data
+    
+workbooks_data = fetch_workbooks_and_views()
+print(workbooks_data)
+workbook_names = list(workbooks_data.keys())
+
+# User selects a workbook.
+st.sidebar.subheader("Select Workbook")
+selected_workbook_name = st.sidebar.selectbox("Workbook", workbook_names)
+
+# Get views for the selected workbook.
+selected_views = workbooks_data['']
+view_names = [view.name for view in selected_views]
+
+# User selects a view.
+st.sidebar.subheader("Select View")
+selected_view_name = st.sidebar.selectbox("View", view_names)
+
+# Fetch selected view details.
+@st.cache_data(ttl=600)
+def fetch_view_details(selected_workbook_name, selected_view_name):
+    with server.auth.sign_in(tableau_auth):
+        selected_view = next(
+            view for view in workbooks_data[selected_workbook_name] if view.name == selected_view_name
+        )
+        
+        server.views.populate_image(selected_view)
+        server.views.populate_csv(selected_view)
+
+        view_image = selected_view.image
+        view_csv = b"".join(selected_view.csv).decode("utf-8")
+        
+        return view_image, view_csv
+
+view_image, view_csv = fetch_view_details(selected_workbook_name, selected_view_name)
+
+# Display the results.
+st.subheader("📓 Workbooks")
+st.write("Available workbooks:", ", ".join(workbook_names))
+
+st.subheader("👁️ Views")
+st.write(f"Workbook *{selected_workbook_name}* has the following views:", ", ".join(view_names))
+
+st.subheader("🖼️ Image")
+st.write(f"Here's what view *{selected_view_name}* looks like:")
+st.image(view_image, width=600)
