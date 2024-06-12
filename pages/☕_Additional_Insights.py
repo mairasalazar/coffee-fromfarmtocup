5d427c725a36eabf8e0aa9ddbbeeb3d050a57103
Maira Salazar <47562677+mairasalazar@users.noreply.github.com>
2024-06-12 22:03:02 +0000

    Add images

--- pages/☕_Additional_Insights.py
+++ pages/☕_Additional_Insights.py
@@ -14,7 +14,8 @@ st.subheader("""There are so many more things to discover and explore about coff
 st.write("If you are looking for the home of the overall best coffee according to the Coffee Quality Institute, you can't go wrong with Ethiopian coffee. Curiously enough, it is in this same country that most type 2 defects are found. ")
 st.write("Type 2 defects are subtle defects that can only be detected through tasting, and include staleness, over-fermentation and rancidness. Type One defects, meanwhile, are more obvious, as they can be perceived through visual inspection. These defects include black beans, insect-damaged or fungus-damaged beans, etc.")
 st.write("What about Brazil, the largest coffee exporter in the world? In this ranking, it doesn't look or taste so good. Maybe it has something to do with its average lower altitude than some of the other producing countries? It does seem that altitude influences coffee profile, and although studies are not definitive and many factors interact to make good coffee, [this study](https://pubmed.ncbi.nlm.nih.gov/29433216/) indicates that sucrose content may be affected by altitude.")
-        
+
+st.image("images/Characteristics.png")  
 components.html(
     f"""<script type='module' src='https://prod-uk-a.online.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js'></script><tableau-viz id='tableau-viz' src='https://prod-uk-a.online.tableau.com/t/romanrodriguezperez3440ff744c/views/CleanVisualizations/Characteristics' width='1024' height='808' hide-tabs toolbar='bottom' ></tableau-viz>""",
     width=1200, height=790
@@ -24,12 +25,14 @@ st.write("""Now, if you are interested in knowing if your coffee comes from a co
          The EUropean Union will implement the The European Deforestation Regulation in December 2024, which will require companies to prove that products like coffee are[ not linked with deforestation](https://apnews.com/article/vietnam-coffee-deforestation-eu-20e3fac82a42beb38013980fa7a760e6).
          This graph looks at the rate of tree cover loss, the percentage of certified coffee over all produced coffee in a country, and total production. There seems to be no relation between certification and deforestation, but given coffee's relatively small role in deforestation, that is to be expected. If you are looking for the home of the overall best coffee according to the Coffee Quality Institute, you can't go wrong with Ethiopian coffee. Curiously enough, it is in this same country that most type 2 defects are found. 
 """)
+st.image("images/Deforestation.png")  
 components.html(
     f"""<script type='module' src='https://prod-uk-a.online.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js'></script><tableau-viz id='tableau-viz' src='https://prod-uk-a.online.tableau.com/t/romanrodriguezperez3440ff744c/views/CertificationDeforestation/Deforestation' width='1024' height='808' hide-tabs toolbar='bottom' ></tableau-viz>""",
     width=1200, height=790
 )
 
 st.write("""In case you are curious, this is how big exporters rank in terms of forest area.""")
+st.image("images/Exports Vs Forest Area.png")  
 components.html(
     f"""<script type='module' src='https://prod-uk-a.online.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js'></script><tableau-viz id='tableau-viz' src='https://prod-uk-a.online.tableau.com/t/romanrodriguezperez3440ff744c/views/CleanVisualizations/ExportsVsForestArea' width='1024' height='808' hide-tabs toolbar='bottom' ></tableau-viz>""",
     width=1200, height=790
@@ -37,6 +40,7 @@ components.html(
 
 st.write("""This brings us to our final visualization, on a more personal level. In many cultures, annedoctally, there seems to be a correlation between coffee and tobacco consumption. Is that really the case? And is it a cultural correlation? case you are curious, this is how big exporters rank in terms of forest area.
          This graph shows us that it seems there isn't a correlation when you account for the whole world, however in some regions, the combo coffee+cigarrete is more prevalent - especially in Europe!""")
+st.image("images/Coffee-Tobacco Consumption Corr.png") 
 components.html(
     f"""<script type='module' src='https://prod-uk-a.online.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js'></script><tableau-viz id='tableau-viz' src='https://prod-uk-a.online.tableau.com/t/romanrodriguezperez3440ff744c/views/CleanVisualizations/Coffee-TobaccoConsumptionCorr' width='1024' height='808' hide-tabs toolbar='bottom' ></tableau-viz>""",
     width=1200, height=790
